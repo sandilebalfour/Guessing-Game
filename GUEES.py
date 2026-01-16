faa3fcb8a1ebe8cd8c019c95fe79558205e79b3e
mbalichantel9 <madibachantel9@gmail.com>
2026-01-16 11:26:25 +0200

    Enjoy the game

---
+++ GUEES.py
@@ -0,0 +1,28 @@
+import random
+
+computer = random.randint(1, 50)
+
+guesses = []
+
+player = int(input("enter a number between 1 -  50: "))
+guesses.append(player)
+
+while player != computer:
+    if player > computer:
+        print('Number is too high')
+
+    elif player < computer:
+        print('Number is too low')
+    
+    elif len(guesses) >= 5:
+        print("You have reached your limit for guesses!")
+        break
+
+    
+    player = int(input("enter a number between 1 -  50: "))
+    guesses.append(player)
+    if player == computer:
+        print('You have guessed it right')
+        
+    
+print(f"your guesses {guesses}")
